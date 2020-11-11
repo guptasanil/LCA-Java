3245d27aceb0f6f6ce3e776deb33d9ed9b158e57
guptasanil <guptasanil2504@gmail.com>
2020-11-11 01:28:49 +0000

    fixed LCA

--- Lowest Common Ancestor/src/lca.java
+++ Lowest Common Ancestor/src/lca.java
@@ -1,22 +1,16 @@
-
 public class lca {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	
-		
-	}
-
 	public static char getLCA(Node a, Node b) {
-		 if ((a.getParent() != null) && (b.getParent() != null)) {
-	            if (a.getParent() == b.getParent()) {
-	                return a.getParentData();
-	            } else if (a.getlevel() < b.getlevel()){
-	                return getLCA(a, b.getParent());
-	            } else {
-	                return getLCA(b, a.getParent());
-	            }
-	        }
-	        return '0';
-	    }
+		if (a.getParent() == b.getParent()) {
+			return a.getParentData();
+		} else if(a == b.getParent()) {
+			return b.getParentData();
+		} else if(b == a.getParent()) {
+			return a.getParentData();
+		} else if (a.getlevel() < b.getlevel()) {
+			return getLCA(a, b.getParent());
+		} else {
+			return getLCA(b, a.getParent());
+		}
+	}
 }
