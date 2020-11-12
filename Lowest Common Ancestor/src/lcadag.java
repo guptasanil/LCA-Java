26d10fad1ca9f7b9fc093e6ff9a7ee335b9405b8
guptasanil <guptasanil2504@gmail.com>
2020-11-12 15:45:14 +0000

    added functions to dag

--- Lowest Common Ancestor/src/lcadag.java
+++ Lowest Common Ancestor/src/lcadag.java
@@ -7,7 +7,7 @@ public class lcadag {
 	private int vertnum;
 	private int edgnum;
 	private ArrayList<Integer>[] nodes;
-	private int[] indegree;
+	private int[] adjnum;
 	private boolean done[];
 	private boolean isAcylic;
 	private boolean order[];
@@ -16,12 +16,12 @@ public class lcadag {
 
 	public lcadag(int vertnum) {
 		if (vertnum < 0)
-			throw new IllegalArgumentException("The number of vertices in the graph must be greater than 0");
+			throw new IllegalArgumentException("There can not be a graph with less than 0 verticies.");
 
 		this.vertnum = vertnum;
 		this.edgnum = 0;
 		this.nodes = (ArrayList<Integer>[]) new ArrayList[vertnum];
-		this.indegree = new int[vertnum];
+		this.adjnum = new int[vertnum];
 		this.done = new boolean[vertnum];
 		this.order = new boolean[vertnum];
 
@@ -47,4 +47,105 @@ public class lcadag {
 		}
 
 	}
+	public boolean addEdge(int from, int to){
+        if (doesvertfit(from) && doesvertfit(to)){
+            nodes[from].add(to);  
+            adjnum[to]++;    
+            edgnum++;
+            return true;
+        }else{
+            System.out.println("Must enter vertices which exist");
+            return false;
+        }
+    }
+
+
+    
+    public int edgin(int v){
+        if (!doesvertfit(v)){
+            return -1;
+        } else {
+            return this.adjnum[v];
+        }
+    }
+    
+    public int edgout(int v){
+        if (!doesvertfit(v)){
+            return -1;
+        } else {
+            return this.nodes[v].size();
+        }
+    }
+
+   
+    public Iterable<Integer> adj(int v){
+        return nodes[v];
+    }
+
+    
+    public boolean isAcyclic(){ 
+    	return this.isAcylic;
+    	}
+    
+    public boolean findCycle(int v){
+        done[v] = true;
+        order[v] = true;
+
+        for (int i : adj(v)){
+            if (!done[i]){
+                findCycle(i);
+            } 
+            else if (order[i]){
+                
+                isAcylic = true;
+                return true;    
+            }
+        }
+
+        order[v] = false;
+        return false;
+    }
+
+  
+    public ArrayList<Integer> Search(int source){
+        boolean visited[] = new boolean[this.vertnum];
+
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        ArrayList<Integer> order = new ArrayList<Integer>();
+
+        visited[source] = true;
+        queue.add(source);
+
+        while (queue.size()!=0){
+            source = queue.poll();
+            order.add(source); 
+            
+            
+            Iterator<Integer> iter = adj(source).iterator();
+            while (iter.hasNext()){
+                int node = iter.next();
+                if (!visited[node]){
+                    visited[node] = true;
+                    queue.add(node);
+                }
+            }
+
+        }
+
+        
+        return order;
+    }
+
+    
+  
+    public lcadag reverse(){
+        lcadag reversedDAG = new lcadag(this.vertnum);
+        for (int i = 0; i < this.vertnum; i++){
+            for (int j: adj(i)) {
+                reversedDAG.addEdge(j, i); 
+            }
+        }
+        return reversedDAG;
+    }
+
 }
