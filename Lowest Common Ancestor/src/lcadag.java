dcf0753256ab75d5342688226c1b1705f5bd18bc
guptasanil <guptasanil2504@gmail.com>
2020-11-12 01:17:51 +0000

    added new dag class

---
+++ Lowest Common Ancestor/src/lcadag.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class lcadag {
+    private int numofvert; 
+    private int numofedg; 
+    private ArrayList<Integer>[] nodes; 
+    private int[] indegree; 
+    private boolean done[]; 
+    private boolean isAcylic; 
+    private boolean visited[]; 
+    private int[] path; 
+    private int[] route; 
+    
+}
+   
