dce47b3c2ba1f28993d13a38be6dce5c2628b0e4
guptasanil <guptasanil2504@gmail.com>
2020-11-12 18:17:55 +0000

    original changes

--- Lowest Common Ancestor/src/Node.java
+++ Lowest Common Ancestor/src/Node.java
@@ -1,34 +1,13 @@
-
 public class Node {
-	
-	private char data;
-	private Node parent;
-	private int level;
-	
-	Node(char data, Node parent){
-		this.data = data;
-		this.parent = parent;
-		if(this.parent == null) {
-			this.level = 0;
-		}
-		else {
-			this.level = this.parent.getlevel() + 1; 
-		}
-		
-	}
 
-	public int getlevel() {
-		return this.level;
-	}
-	
-	public char getParentData() {
-		return parent.data;
-	}
-	
-	public Node getParent() {
-        return this.parent;
-    }
+	int value;
+	Node right;
+	Node left;
 
-	
-	
+	Node(int value) {
+		this.value = value;
+		this.right = null;
+		this.left = null;
+
+	}
 }
