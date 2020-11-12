bb10bb5112560dbf6a213c646102b06c4921da0c
guptasanil <guptasanil2504@gmail.com>
2020-11-12 14:48:34 +0000

    working on dag class

--- Lowest Common Ancestor/src/lcadag.java
+++ Lowest Common Ancestor/src/lcadag.java
@@ -4,15 +4,47 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class lcadag {
-    private int numofvert; 
-    private int numofedg; 
-    private ArrayList<Integer>[] nodes; 
-    private int[] indegree; 
-    private boolean done[]; 
-    private boolean isAcylic; 
-    private boolean visited[]; 
-    private int[] path; 
-    private int[] route; 
-    
+	private int vertnum;
+	private int edgnum;
+	private ArrayList<Integer>[] nodes;
+	private int[] indegree;
+	private boolean done[];
+	private boolean isAcylic;
+	private boolean order[];
+	private int[] path;
+	private int[] route;
+
+	public lcadag(int vertnum) {
+		if (vertnum < 0)
+			throw new IllegalArgumentException("The number of vertices in the graph must be greater than 0");
+
+		this.vertnum = vertnum;
+		this.edgnum = 0;
+		this.nodes = (ArrayList<Integer>[]) new ArrayList[vertnum];
+		this.indegree = new int[vertnum];
+		this.done = new boolean[vertnum];
+		this.order = new boolean[vertnum];
+
+
+		for (int i = 0; i < vertnum; i++) {
+			nodes[i] = new ArrayList<Integer>();
+		}
+	}
+
+	public int getnumofedg() {
+		return edgnum;
+	}
+
+	public int getnumofvert() {
+		return vertnum;
+	}
+
+	public boolean doesvertfit(int v) {
+		if (v > 0 && v <= vertnum) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 }
-   
